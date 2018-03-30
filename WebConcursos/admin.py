cae94a9f671ebd465a4c2e4a6873643ef2437c8e
YEISON JAVIER <yjc1984@gmail.com>
2018-03-30 16:44:54 -0500

    Creacion proyecto 3

---
+++ WebConcursos/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(UsuarioCustom)
+admin.site.register(Concurso)
+admin.site.register(AudioLocutor)
+admin.site.register(Locutor)
+admin.site.register(ListaLocutores)
+admin.site.register(EmpresaRol)
